e5abe9fe273402856744341b893a0bc033e0fac0
alfredojg2804-ctrl <alfredojg2804@gmail.com>
2026-02-04 13:28:29 -0600

    add login page with autentication

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login page</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <h2>Aqui va el login JEJE</h2>
+</body>
+</html>
